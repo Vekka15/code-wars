81d9b7b95afe54ff82cab4a119122f04d0e2a7ef
Vekka15 <vekka15@gmail.com>
2015-05-10 21:26:27 +0200

    remove

--- cw1.rb
+++
@@ -1,7 +0,0 @@
-zmienna = "Biedronka"
-
-puts "#{zmienna}"
-
-zmienna1 = zmienna.slice(0,3)
-
-puts "#{zmienna1}"
--- cwP.rb
+++
@@ -1,8 +0,0 @@
-
-array = [1,2,3]
-
-new_array = array.collect{|x| x+2}
-
-new_array.each_with_index do |item, index|
-puts "#{index} element to #{item}"
-end
